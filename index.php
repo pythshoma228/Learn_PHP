f59176f03b64f3500936ceedc85d8d99efd53464
ShMg <32521952+pythshoma228@users.noreply.github.com>
2019-06-15 18:42:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 	<title>web</title>
 </head>
 <body>
-	<form method="POST" action="sayhello.php">
-		Имя:
-		<input type="text" name="user"><br>
-		<input type="submit"value="Отправить">
+<form method="POST" action="sayhello.php">
+	Имя:
+	<input type="text" name="user"><br>
+	<input type="submit"value="Отправить">
 	</form>
 </body></html>
