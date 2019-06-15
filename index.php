ea018b15a3c4dadb3ee9a8b1b9c1d38ba5d21e3d
ShMg <32521952+pythshoma228@users.noreply.github.com>
2019-06-15 18:30:28 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>web</title>
+</head>
+<body>
+	<form method="POST" action="sayhello.php">
+		Имя:
+		<input type="text" name="user">
+		<br>
+		<input type="submit" value="Отправить">
+	</form>
+</body>
+</html>
